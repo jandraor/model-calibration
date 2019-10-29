a232aed4d0e33b6b6b9eec14e5f7f049e86060e5
Jair Andrade <jair.albert.andrade@gmail.com>
2019-10-29 18:12:20 +0000

    Adding more explorations

--- deterministic_models/SEIR.R
+++ deterministic_models/SEIR.R
@@ -1,7 +1,7 @@
 
 
-SIR <- function(initInfected, effectiveContacts, recoveryTime,
-                               initSusceptible = 990,
+SEIR <- function(initInfected, effectiveContacts, recoveryTime, latentPeriod,
+                initSusceptible = 990,
                 start = 0,
                 finish = 30){
   
@@ -19,49 +19,58 @@ SIR <- function(initInfected, effectiveContacts, recoveryTime,
   
   # Create stocks vector, with initial values
   stocks <- c(Susceptible = initSusceptible,
+              Exposed     = 0,
               Infected    = initInfected,
               Recovered   = 0)
   
-
+  
   
   
   # Create auxiliaries vector, with values
-  auxs    <- c(effectiveContacts = effectiveContacts, 
-               recoveryTime = recoveryTime)
+  auxs    <- c(effectiveContacts = effectiveContacts,
+               latentPeriod      = latentPeriod,
+               recoveryTime      = recoveryTime)
   
   model <- function (time, stocks, auxs) 
     with(as.list(c(stocks, auxs)), {
       
-      population           <- Susceptible + Infected + Recovered
+      population           <- Susceptible + Exposed + Infected + Recovered
       probability          <- Susceptible / population
       contactsPerInfected  <- Infected * effectiveContacts
+      
       #Flows
-      RR                   <- Infected / recoveryTime
       IR                   <- contactsPerInfected  * probability
+      InR                  <- Exposed / latentPeriod
+      RR                   <- Infected / recoveryTime
+      
       # Net flows
       d_Susceptible_dt  <- -IR
-      d_Infected_dt     <- IR - RR
+      d_Exposed_dt      <- IR - InR
+      d_Infected_dt     <- InR - RR
       d_Recovered_dt    <- RR
       
-      return(list(c(d_Susceptible_dt, d_Infected_dt, d_Recovered_dt), 
-                  IR = IR, 
+      return(list(c(d_Susceptible_dt, d_Exposed_dt, 
+                    d_Infected_dt, d_Recovered_dt), 
+                  IR = IR,
+                  InR = InR,
                   RR = RR, 
                   probability = probability, 
                   contactsPerInfected = contactsPerInfected, 
                   effectiveContacts = effectiveContacts, 
                   population = population, 
-                  recoveryTime = recoveryTime))
+                  recoveryTime = recoveryTime,
+                  latentPeriod = latentPeriod))
     })
   
   # Call Solver, and store results in a data frame
   o <- data.frame(ode(y = stocks, times = simtime, func = model,
                       parms = auxs, method = "rk4"))
   
-  tidy_sir <- pivot_longer(o, -time,
-                            names_to = "variable", values_to = "value") %>%
+  tidy_seir <- pivot_longer(o, -time,
+                           names_to = "variable", values_to = "value") %>%
     filter(variable %in% names(stocks))
-
-  g <- ggplot(tidy_sir, aes(x = time, y = value)) +
+  
+  g <- ggplot(tidy_seir, aes(x = time, y = value)) +
     geom_line(aes(group = variable, colour = variable))
   
   incidence_data <- o %>% select(time, names(stocks)) %>% 
@@ -70,8 +79,7 @@ SIR <- function(initInfected, effectiveContacts, recoveryTime,
            incidence = cumulativeInfected - lag(cumulativeInfected, 
                                                 default = cumulativeInfected[1]),
            incidence = round(incidence)) %>% 
-    select(time, incidence) %>% 
-    filter(time != 0)
+    select(time, incidence) %>% filter(time != 0)
   
   list(incidence_data = incidence_data,
        model_df   = o,
